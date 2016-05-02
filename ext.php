a0507d29148498a3a916b1499cafa00dc532d7bf
david63 <david.wood@pear-tree.org.uk>
2016-05-02 16:13:26 +0100

    Re commit

---
+++ ext.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+*
+* @package User Ranks Extension
+* @copyright (c) 2015 david63
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace david63\userranks;
+
+/**
+* Extension class for stop forum spam
+*/
+
+class ext extends \phpbb\extension\base
+{
+	const USER_RANKS_VERSION	= '2.1.0';
+
+	/**
+	* Enable extension if phpBB version requirement is met
+	*
+	* @var string Require 3.2.0-a1 due to updated 3.2 syntax
+	*
+	* @return bool
+	* @access public
+	*/
+	public function is_enableable()
+	{
+		$config = $this->container->get('config');
+
+		if (!phpbb_version_compare($config['version'], '3.2.0-a1', '>='))
+		{
+			$this->container->get('language')->add_lang('ext_userranks', 'david63/userranks');
+			trigger_error($this->container->get('language')->lang('VERSION_32') . adm_back_link(append_sid('index.' . $this->container->getParameter('core.php_ext'), 'i=acp_extensions&amp;mode=main')), E_USER_WARNING);
+		}
+		else
+		{
+			return true;
+		}
+	}
+}
